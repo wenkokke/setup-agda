a7168b257b8ff25da5d4e771efd30794083476b2
Wen Kokke <wenkokke@users.noreply.github.com>
2022-09-30 19:45:14 +0100

    Add haskell setup.

--- jest.config.js
+++ jest.config.js
@@ -2,6 +2,7 @@ module.exports = {
   clearMocks: true,
   moduleFileExtensions: ['js', 'ts'],
   testMatch: ['**/*.test.ts'],
+  testPathIgnorePatterns: ['<rootDir>/node_modules/', '<rootDir>/vendor/'],
   transform: {
     '^.+\\.ts$': 'ts-jest'
   },
