084b815d977bc12e9e434e82e59ef2327b9b72d4
Wen Kokke <wenkokke@users.noreply.github.com>
2022-09-30 19:03:41 +0100

    Print info

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,7 @@ import * as core from '@actions/core'
 async function run(): Promise<void> {
   try {
     const agda_version: string = core.getInput('agda-version')
-    core.debug(agda_version)
+    core.info(agda_version)
   } catch (error) {
     if (error instanceof Error) core.setFailed(error.message)
   }
