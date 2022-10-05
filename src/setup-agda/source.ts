57c8c2a95670333d41d973a0ccb0ecf6c575b6a1
Wen Kokke <wenkokke@users.noreply.github.com>
2022-10-05 14:44:47 +0100

    Get package from Hackage

--- src/setup-agda/source.ts
+++ src/setup-agda/source.ts
@@ -1,6 +1,8 @@
 import * as core from '@actions/core'
 import * as config from '../util/config'
+import * as path from 'path'
 import {cabal, getCabalVersion} from '../setup-haskell'
+import {execOutput} from '../util/exec'
 
 export async function buildAgda(version?: string): Promise<void> {
   const packageName = version === undefined ? 'Agda' : `Agda-${version}`
@@ -15,4 +17,7 @@ export async function buildAgda(version?: string): Promise<void> {
   //
   core.info(`Get ${packageName} from Hackage`)
   await cabal(['get', packageName, '--destdir', config.cacheDir])
+  const sourceDir = path.join(config.cacheDir, packageName)
+  const output = await execOutput('ls', ['-R', sourceDir])
+  core.info(output)
 }
