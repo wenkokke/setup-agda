d689da3bd9095588153aa9f9e5f667b7f7ef2d78
Wen Kokke <wenkokke@users.noreply.github.com>
2023-02-02 23:49:01 +0000

    Less verbose testing

--- src/cli/test.ts
+++ src/cli/test.ts
@@ -7,13 +7,13 @@ export default async function test(
   options?: Partial<AgdaOptions> & ExecOptions
 ): Promise<void> {
   const versionString = await agda.getVersion(options)
-  logger.info(`Found Agda version ${versionString} on PATH`)
+  logger.debug(`Found Agda version ${versionString}`)
   const dataDir = await agda.getDataDir(options)
-  logger.info(`Found Agda data directory at ${dataDir}`)
+  logger.debug(`Found Agda data directory at ${dataDir}`)
   for (const agdaFile of glob.sync(
     path.join(dataDir, 'lib', 'prim', '**', '*.agda')
   )) {
-    logger.info(`Compiling ${agdaFile}`)
+    logger.debug(`Compiling ${agdaFile}`)
     await agda(['-v0', agdaFile], {
       ...options,
       cwd: path.join(dataDir, 'lib', 'prim')
