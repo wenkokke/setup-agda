285a2b455598d1b9da58537f87da44047661dd2c
Wen Kokke <wenkokke@users.noreply.github.com>
2024-04-09 19:17:40 +0100

    Fix build-cli-exe

--- scripts/build-cli-exe.mjs
+++ scripts/build-cli-exe.mjs
@@ -4,6 +4,6 @@ import pkg from 'pkg'
 pkg.exec([
   './dist/cli.cjs',
   '--compress=GZip',
-  '--target=node16-linux-x64,node16-macos-x64,node16-win-x64',
+  '--target=node16-linux-x64,node16-macos-x64,node16-win-x64,node16-linux-arm64,node16-macos-arm64,node16-win-arm64',
   '--output=./dist/agdaup'
 ])
